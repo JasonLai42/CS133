2fa8a1f1c5a4391af6e2bb282891a82853aee603
Ubuntu <ubuntu@ip-172-31-70-118.ec2.internal>
2021-01-19 07:30:26 +0000

    parallelized for now

--- Project1/lab1/omp.cpp
+++ Project1/lab1/omp.cpp
@@ -10,12 +10,17 @@
 void GemmParallel(const float a[kI][kK], const float b[kK][kJ],
                   float c[kI][kJ]) {
   // Your code goes here...
+
+  #pragma omp parallel for num_threads(8)
   for (int i = 0; i < kI; ++i) {
     std::memset(c[i], 0, sizeof(float) * kJ);
   }
-  for (int k = 0; k < kK; k += 4) {
-    for (int i = 0; i < kI; ++i) {
-      for (int j = 0; j < kJ; ++j) {
+
+  int k, i, j;
+  for (k = 0; k < kK; k += 4) {
+    #pragma omp parallel for num_threads(8) private(j)
+    for (i = 0; i < kI; ++i) {
+      for (j = 0; j < kJ; ++j) {
         c[i][j] += a[i][k] * b[k][j];
         c[i][j] += a[i][k+1] * b[k+1][j];
         c[i][j] += a[i][k+2] * b[k+2][j];
