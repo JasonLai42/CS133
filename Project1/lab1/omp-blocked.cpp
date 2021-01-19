bb835c9df7a17a9336c580e8837445a6fcb61e76
JasonLai42 <jasonlaiusa@yahoo.com>
2021-01-19 03:26:39 -0800

    blocked gemm

--- Project1/lab1/omp-blocked.cpp
+++ Project1/lab1/omp-blocked.cpp
@@ -10,13 +10,24 @@
 void GemmParallelBlocked(const float a[kI][kK], const float b[kK][kJ],
                          float c[kI][kJ]) {
   // Your code goes here...
+  
+  int block_size = 256;
+
   for (int i = 0; i < kI; ++i) {
     std::memset(c[i], 0, sizeof(float) * kJ);
   }
-  for (int i = 0; i < kI; ++i) {
-    for (int j = 0; j < kJ; ++j) {
-      for (int k = 0; k < kK; ++k) {
-        c[i][j] += a[i][k] * b[k][j];
+
+  for (int i = 0; i < kI; i += block_size) {
+    for (int j = 0; j < kJ; j += block_size) {
+      for (int k = 0; k < kK; k += 4) {
+        for(int ii = i; ii < i + block_size; ++ii) {
+          for(int jj = j; jj < j + block_size; ++jj) {
+            c[ii][jj] += a[ii][k] * b[k][jj];
+            c[ii][jj] += a[ii][k+1] * b[k+1][jj];
+            c[ii][jj] += a[ii][k+2] * b[k+2][jj];
+            c[ii][jj] += a[ii][k+3] * b[k+3][jj];
+          }
+        }
       }
     }
   }
