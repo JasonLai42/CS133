74606e3f7d26e6fd56478d32b63badb65e95a107
Ubuntu <ubuntu@ip-172-31-70-118.ec2.internal>
2021-01-19 20:29:57 +0000

    Parallelized blocked

--- Project1/lab1/omp-blocked.cpp
+++ Project1/lab1/omp-blocked.cpp
@@ -11,17 +11,20 @@ void GemmParallelBlocked(const float a[kI][kK], const float b[kK][kJ],
                          float c[kI][kJ]) {
   // Your code goes here...
   
-  int block_size = 256;
+  int block_size = 128;
 
+  #pragma omp parallel for num_threads(8)
   for (int i = 0; i < kI; ++i) {
     std::memset(c[i], 0, sizeof(float) * kJ);
   }
 
-  for (int i = 0; i < kI; i += block_size) {
-    for (int j = 0; j < kJ; j += block_size) {
-      for (int k = 0; k < kK; k += 4) {
-        for(int ii = i; ii < i + block_size; ++ii) {
-          for(int jj = j; jj < j + block_size; ++jj) {
+  int i, j, k, ii, jj;
+  #pragma omp parallel for num_threads(8) schedule(guided)
+  for (i = 0; i < kI; i += block_size) {
+    for (j = 0; j < kJ; j += block_size) {
+      for (k = 0; k < kK; k += 4) {
+        for(ii = i; ii < i + block_size; ++ii) {
+          for(jj = j; jj < j + block_size; ++jj) {
             c[ii][jj] += a[ii][k] * b[k][jj];
             c[ii][jj] += a[ii][k+1] * b[k+1][jj];
             c[ii][jj] += a[ii][k+2] * b[k+2][jj];
--- Project1/lab1/omp.cpp
+++ Project1/lab1/omp.cpp
@@ -11,14 +11,14 @@ void GemmParallel(const float a[kI][kK], const float b[kK][kJ],
                   float c[kI][kJ]) {
   // Your code goes here...
 
-  #pragma omp parallel for num_threads(8) shared(c)
+  #pragma omp parallel for num_threads(8)
   for (int i = 0; i < kI; ++i) {
     std::memset(c[i], 0, sizeof(float) * kJ);
   }
 
   int k, i, j;
   for (k = 0; k < kK; k += 4) {
-    #pragma omp parallel for num_threads(8) shared(c) schedule(guided)
+    #pragma omp parallel for num_threads(8) schedule(guided)
     for (i = 0; i < kI; ++i) {
       for (j = 0; j < kJ; ++j) {
         c[i][j] += a[i][k] * b[k][j];
