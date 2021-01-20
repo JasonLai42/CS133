79ed16c147cffb5dadfdf0968c5c29176ad9d7e8
Ubuntu <ubuntu@ip-172-31-70-118.ec2.internal>
2021-01-20 08:32:35 +0000

    tiled all 3 loops

--- Project1/lab1/omp-blocked.cpp
+++ Project1/lab1/omp-blocked.cpp
@@ -18,14 +18,16 @@ void GemmParallelBlocked(const float a[kI][kK], const float b[kK][kJ],
     std::memset(c[i], 0, sizeof(float) * kJ);
   }
 
-  int i, j, k, ii, jj;
-  #pragma omp parallel for num_threads(8) private(j, k, ii, jj) schedule(guided)
+  int i, j, k, ii, jj, kk;
+  #pragma omp parallel for num_threads(8) private(k, j, kk, ii, jj) schedule(guided)
   for (i = 0; i < kI; i += block_size) {
-    for (j = 0; j < kJ; j += block_size) {
-      for (k = 0; k < kK; k += 4) {
-        for(ii = i; ii < i + block_size; ++ii) {
-          for(jj = j; jj < j + block_size; ++jj) {
-            c[ii][jj] += (a[ii][k] * b[k][jj]) + (a[ii][k+1] * b[k+1][jj]) + (a[ii][k+2] * b[k+2][jj]) + (a[ii][k+3] * b[k+3][jj]);
+    for (k = 0; k < kK; k += block_size) {
+      for (j = 0; j < kJ; j += block_size) {
+        for(kk = k; kk < k + block_size; kk += 4) {
+          for(ii = i; ii < i + block_size; ++ii) {
+            for(jj = j; jj < j + block_size; ++jj) {
+              c[ii][jj] += (a[ii][kk] * b[kk][jj]) + (a[ii][kk+1] * b[kk+1][jj]) + (a[ii][kk+2] * b[kk+2][jj]) + (a[ii][kk+3] * b[kk+3][jj]);
+            }
           }
         }
       }
