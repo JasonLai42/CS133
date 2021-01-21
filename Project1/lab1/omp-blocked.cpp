73624a6504a7fd35723427b21b9abd1a4d0a9142
Ubuntu <ubuntu@ip-172-31-70-118.ec2.internal>
2021-01-21 20:55:36 +0000

    different tiling sizes for different loops

--- Project1/lab1/omp-blocked.cpp
+++ Project1/lab1/omp-blocked.cpp
@@ -11,7 +11,9 @@ void GemmParallelBlocked(const float a[kI][kK], const float b[kK][kJ],
                          float c[kI][kJ]) {
   // Your code goes here...
   
-  int block_size = 128;
+  int block_sizeI = 32;
+  int block_sizeJ = 512;
+  int block_sizeK = 128;
 
   #pragma omp parallel for num_threads(8)
   for (int i = 0; i < kI; ++i) {
@@ -20,12 +22,12 @@ void GemmParallelBlocked(const float a[kI][kK], const float b[kK][kJ],
 
   int i, j, k, ii, jj, kk;
   #pragma omp parallel for num_threads(8) private(k, j, kk, ii, jj)
-  for (i = 0; i < kI; i += block_size) {
-    for (k = 0; k < kK; k += block_size) {
-      for (j = 0; j < kJ; j += block_size) {
-        for(kk = k; kk < k + block_size; kk += 4) {
-          for(ii = i; ii < i + block_size; ++ii) {
-            for(jj = j; jj < j + block_size; ++jj) {
+  for (i = 0; i < kI; i += block_sizeI) {
+    for (k = 0; k < kK; k += block_sizeK) {
+      for (j = 0; j < kJ; j += block_sizeJ) {
+        for(kk = k; kk < k + block_sizeK; kk += 4) {
+          for(ii = i; ii < i + block_sizeI; ++ii) {
+            for(jj = j; jj < j + block_sizeJ; ++jj) {
               c[ii][jj] += (a[ii][kk] * b[kk][jj]) 
                         + (a[ii][kk+1] * b[kk+1][jj]) 
                         + (a[ii][kk+2] * b[kk+2][jj]) 
