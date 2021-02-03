f2c459f6b0beeb25b425767e3c1bc4a38259d506
JasonLai42 <jasonlaiusa@yahoo.com>
2021-02-02 22:34:27 -0800

    aws test now

--- Project2/mpi.cpp
+++ Project2/mpi.cpp
@@ -1,5 +1,7 @@
 // Header inclusions, if any...
 
+#include <stdlib.h>
+#include <string.h>
 #include <mpi.h>
 
 #include "lib/gemm.h"
@@ -12,4 +14,67 @@
 void GemmParallelBlocked(const float a[kI][kK], const float b[kK][kJ],
                          float c[kI][kJ]) {
   // Your code goes here...
+
+  // Tile sizes
+  int block_sizeI = 32;
+  int block_sizeJ = 512;
+  int block_sizeK = 128;
+
+  // Get ranks and number of processes
+  int rank, num_proc;
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &num_proc);
+
+  int chunk_size = kI / num_proc;
+
+  // Send buffers
+  float *sendA = (float*)lab2::aligned_alloc(4096, kI * kK * sizeof(float));
+  float *sendB = (float*)lab2::aligned_alloc(4096, kK * kJ * sizeof(float));
+  if(rank == 0) {
+    std::memcpy(sendA, a, kI * kK * sizeof(float));
+    std::memcpy(sendB, b, kK * kJ * sizeof(float));
+  }
+  
+  // Store computed rows of c
+  float *productC = (float*)lab2::aligned_alloc(4096, chunk_size * kJ * sizeof(float));
+
+  // Receive buffers
+  float *recA = (float*)lab2::aligned_alloc(4096, chunk_size * kK * sizeof(float));
+
+
+  MPI_Scatter(sendA, chunk_size * kK, MPI_FLOAT, recA, chunk_size * kK, MPI_FLOAT, 0, MPI_COMM_WORLD);
+  MPI_Bcast(sendB, kK * kJ, MPI_FLOAT, 0, MPI_COMM_WORLD);
+
+  std::memset(productC, 0, chunk_size * kJ * sizeof(float));
+
+  // for(int ii = 0; ii < chunk_size; ii += 64)
+  //   for(int jj = 0; jj < kJ; jj += 1024)
+  //     for(int kk = 0; kk < kK; kk += 8)
+  //       for(int i = 0; i < 64; i++)
+  //         for(int j = 0; j < 1024; j++) {
+  //           float reg = productC[((i + ii) * kJ) + (j + jj)];
+  //           for(int k = 0; k < 8; k++)
+  //             reg += recA[((i + ii) * kK) + (k + kk)] * sendB[((k + kk) * kJ) + (j + jj)];
+  //           productC[((i + ii) * kJ) + (j + jj)] = reg;
+  //         }
+
+  for (int i = 0; i < chunk_size; i += block_sizeI) {
+    for (int k = 0; k < kK; k += block_sizeK) {
+      for (int j = 0; j < kJ; j += block_sizeJ) {
+        for(int kk = k; kk < k + block_sizeK; kk += 4) {
+          for(int ii = i; ii < i + block_sizeI; ++ii) {
+            for(int jj = j; jj < j + block_sizeJ; ++jj) {
+              productC[(ii * kJ) + jj] += (recA[(ii * kK) + kk] * sendB[(kk * kJ) + jj]) 
+                        + (recA[(ii * kK) + kk + 1] * sendB[((kk + 1) * kJ) + jj]) 
+                        + (recA[(ii * kK) + kk + 2] * sendB[((kk + 2) * kJ) + jj]) 
+                        + (recA[(ii * kK) + kk + 3] * sendB[((kk + 3) * kJ) + jj]);
+            }
+          }
+        }
+      }
+    }
+  }
+
+  // Gather chunks back to process 0
+  MPI_Gather(productC, chunk_size * kJ, MPI_FLOAT, c, chunk_size * kJ, MPI_FLOAT, 0, MPI_COMM_WORLD);
 }
