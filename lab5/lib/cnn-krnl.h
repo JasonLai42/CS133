cf156e086d0e70920eaa0fa89943922b1a286866
JasonLai42 <jasonlaiusa@yahoo.com>
2021-03-10 00:26:35 -0800

    Revert "new header file from TA"
    This reverts commit 222ceef6017920efd39eea8895731e068cc385a5.

--- lab5/lib/cnn-krnl.h
+++ lab5/lib/cnn-krnl.h
@@ -169,15 +169,8 @@ void read_input_from_memory(int hh, int ww,
       const int till       = (end_real - 1) / item_per_read;
 
       for (int idx = start; idx <= till; idx++) {
-#if kTileW <= 56
-  // (kTileW+4) / item_per_read <= 60/64 <= 1
-  #pragma HLS loop_tripcount min=1 max=1 avg=1
-#elif kTileW <= 112
-  // (kTileW+4) / item_per_read <= 116/64 <= 2
-  #pragma HLS loop_tripcount min=2 max=2 avg=2
-#else
-  #pragma HLS loop_tripcount min=3 max=3 avg=3
-#endif
+// (kTileW+4) / item_per_read <= 116/64 <= 2
+#pragma HLS loop_tripcount min=2 max=2 avg=2
         input_g_t data = input_g[idx];
 
         for (int item = 0; item < item_per_read; item++) {
@@ -209,12 +202,8 @@ void write_output_to_memory(int hh, int ww,
       const int till       = (end_real - 1) / item_per_read;
 
       for (int idx = start; idx <= till; idx++) {
-#if kTileW <= 112
-  // (kTileW / 2) / item_per_read <= 56/64 <= 1
-  #pragma HLS loop_tripcount min=1 max=1 avg=1
-#else
-  #pragma HLS loop_tripcount min=2 max=2 avg=2
-#endif
+// (kTileW / 2) / item_per_read <= 56/64 <= 1
+#pragma HLS loop_tripcount min=1 max=1 avg=1
         output_g_t data = output_g[idx];
 
         for (int item = 0; item < item_per_read; item++) {
